a4745d8e3c04b2c9d3019062787fb9a476a7da09
Dan Hemerlein <danhemerlein@gmail.com>
2020-12-02 21:57:31 -0500

    12-2-2020

--- arrays/methods.js
+++ arrays/methods.js
@@ -39,14 +39,58 @@ const meats = {
   pork: 7,
 }
 
-console.log(Object.entries(meats))
-console.log(Object.keys(meats))
-console.log(Object.values(meats))
+// console.log(Object.entries(meats))
+// console.log(Object.keys(meats))
+// console.log(Object.values(meats))
 
-Object.entries(meats).forEach((entry) => {
-  const [key, val] = entry;
-  console.log(entry);
-  console.log(key);
-  console.log(val);
+// Object.entries(meats).forEach((entry) => {
+//   const [key, val] = entry;
+//   console.log(entry);
+//   console.log(key);
+//   console.log(val);
+// })
 
-})
+const buns = ['egg', 'wonder', 'sesame'];
+const lunch = "hot dogs,hamburgers,sausages,corn";
+
+// instance methods
+
+console.log(buns.join(' or '));
+// you can't split something with an emoji
+console.log(lunch.split(','));
+
+// mutable - recall that push and pop are like a stack - like the call stack
+const lastItem = buns.pop();
+// console.log(lastItem);
+// console.log(buns);
+buns.push(lastItem);
+// console.log(buns);
+
+// take the first item off an array with shift
+const firstItem = buns.shift();
+// console.log(firstItem);
+// console.log(buns);
+buns.unshift(firstItem);
+// console.log(buns);
+
+// slice is immutable
+const newBuns = buns.slice(0, buns.length - 1);
+// console.log(newBuns);
+// console.log(buns);
+
+// make a copy of the array with slice
+
+const bunsCopy = buns.slice(0);
+console.log(bunsCopy);
+buns.push('ahhh')
+console.log(buns);
+
+const bunsCopy2 = [...buns];
+console.log(bunsCopy2);
+
+const spl = bunsCopy2.splice(1,2);
+console.log(spl);
+console.log(bunsCopy2);
+
+// reference versus value - something to keep in mind when working with objects
+
